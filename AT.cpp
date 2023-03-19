cf9bb6fbd002cc41fcc002a2974ba1525ff7ac80
sm745052 <79754058+sm745052@users.noreply.github.com>
2023-03-19 20:16:26 +0530

    Create AT.cpp

---
+++ AT.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define F first
+#define S second
+#define PN no()
+#define PY yes()
+void no(){
+    cout<<"No"<<endl;
+}
+void yes(){
+    cout<<"Yes"<<endl;
+}
+// --------------------------------------------------------------------
+
+
+
+
+signed main(){
+  int n, m;
+  cin>>n>>m;
+  for(int i=0;i<n;i++){
+
+  }
+  return 0;
+}
