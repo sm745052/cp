b8b0357daf7b2304b193f2ce8e6e88adfeec7823
sm745052 <79754058+sm745052@users.noreply.github.com>
2023-03-20 01:19:12 +0530

    Update CF.cpp

--- CF.cpp
+++ CF.cpp
@@ -5,13 +5,29 @@ using namespace std;
 #define S second
 #define PN no()
 #define PY yes()
+#define v_inp(a) for(auto &i:a)cin>>i
+#define v_print(a) for(int i=0;i<a.size();i++)cout<<a[i]<<" \n"[i==a.size()-1];
+#define s_print(a) for(auto i:a)cout<<i<<" ";cout<<endl;
+#define pb push_back
+
+template<class T>
+vector<T> v_psum(vector<T> a){
+    vector<T> prefix(a.begin(), a.end());
+    for(int i=1;i<a.size();i++)prefix[i]+=prefix[i-1];
+    return prefix;
+}
+
 void no(){
     cout<<"No"<<endl;
 }
 void yes(){
     cout<<"Yes"<<endl;
 }
+
 // --------------------------------------------------------------------
+
+
+
 
 
 void solve()
@@ -19,9 +35,8 @@ void solve()
     int n;
     cin>>n;
     vector<int>a(n);
-    for(auto &i:a){
-        cin>>i;
-    }
+    v_inp(a);
+    vector<int> p_sum = v_psum(a);
 }
 
 
