9c98959b5a5a7f356bd59dec7398c8d22f0b7cdc
sm745052 <79754058+sm745052@users.noreply.github.com>
2023-03-19 20:15:59 +0530

    Create CF.cpp

---
+++ CF.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define F first
+#define S second
+#define PN no()
+#define PY yes()
+void no(){
+    cout<<"No"<<endl;
+}
+void yes(){
+    cout<<"Yes"<<endl;
+}
+// --------------------------------------------------------------------
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(auto &i:a){
+        cin>>i;
+    }
+}
+
+
+
+
+signed main()
+{
+    int t=1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
