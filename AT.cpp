f282410598826595464efe162f11cdfceb15fa7f
sm745052 <79754058+sm745052@users.noreply.github.com>
2023-03-20 19:26:05 +0530

    Update AT.cpp

--- AT.cpp
+++ AT.cpp
@@ -7,9 +7,12 @@ using namespace std;
 #define PY yes()
 #define v_inp(a) for(auto &i:a)cin>>i
 #define v_print(a) for(int i=0;i<a.size();i++)cout<<a[i]<<" \n"[i==a.size()-1];
-#define s_print(a) for(auto i:a)cout<<i<<" ";cout<<endl;
+#define v_print(a) for(auto i:a)cout<<i<<" ";cout<<endl;
 #define pb push_back
 #define v_sort(a) sort(a.begin(), a.end());
+#define v_lb(a, x) lower_bound(a.begin(), a.end(), x)
+#define v_ub(a, x) upper_bound(a.begin(), a.end(), x)
+#define v_accumulate(a, x) accumulate(a.begin(), a.end(), x)
 #define rep(i, l, r) for(int i=l;i<r;i++)
 template<class T>
 vector<T> v_psum(vector<T> a){
@@ -17,14 +20,14 @@ vector<T> v_psum(vector<T> a){
     for(int i=1;i<a.size();i++)prefix[i]+=prefix[i-1];
     return prefix;
 }
- 
+
 void no(){
     cout<<"No"<<endl;
 }
 void yes(){
     cout<<"Yes"<<endl;
 }
-}
+
 // --------------------------------------------------------------------
 
 
